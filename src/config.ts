d2b10886a8f97c254703765573fe0ad2741d8566
Jack <114013039+jackweath@users.noreply.github.com>
2022-09-22 11:57:48 +0100

    - Added image for embed

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Jack Weatherley";
-export const SITE_DESCRIPTION = "Site for Jack Weatherley";
+export const SITE_TITLE = 'Jack Weatherley';
+export const SITE_DESCRIPTION = 'Software Engineer 🇬🇧';
