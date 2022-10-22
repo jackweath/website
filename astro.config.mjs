10d4be0e0249e68aba82ca8ce8d5f73922f78931
Jack <114013039+jackweath@users.noreply.github.com>
2022-10-22 10:37:37 +0100

    - Added site URL for astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+    site: 'https://jackweath.com',
+    integrations: [tailwind(), sitemap()]
 });
